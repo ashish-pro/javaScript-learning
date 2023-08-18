51c5366041f3145da14cbfd03809c00b62146e3b
ashish-pro <ashishbit017@outlook.com>
2023-08-18 10:01:24 +0530

    variables and identifiers

---
+++ variables_in_JS/identifiers.js
@@ -0,0 +1,22 @@
+// Identifiers: All JavaScript variables must be identified with unique names. These unique names are called identifiers.
+
+
+/*Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+ 1. The general rules for constructing names for variables (unique identifiers) are:
+
+ 2. Names can contain letters, digits, underscores, and dollar signs.
+
+ 3. Names must begin with a letter.
+
+ 4. Names can also begin with $ and _ .
+
+ 5. Names are case sensitive (y and Y are different variables).
+
+ 6. Reserved words (like JavaScript keywords) cannot be used as names.
+ 
+ */
+
+//JavaScript identifiers are case-sensitive.
+
+
---
+++ variables_in_JS/variables.js
@@ -0,0 +1,49 @@
+
+/* Variables: Variables are containers for storing values. */
+
+
+
+// How to Declare a variables in JavaScript?
+
+/* 
+There are three ways to declare variables in JavaScript
+1. Using Var Keyword
+2. Using let Keyword
+3. Using const Keyword
+*/
+
+var price = 450.00;
+let brand = "dell";
+const product_type = "mouse";
+
+// Reading The Variables
+console.log(price);
+console.log(brand);
+console.log(product_type);
+
+/* 
+Note:
+The var keyword was used in all JavaScript code from 1995 to 2015.
+
+The let and const keywords were added to JavaScript in 2015.
+
+The var keyword should only be used in code written for older browsers.
+*/
+
+
+
+/*
+When to Use var, let, or const?
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers.
+*/
+
+
+
